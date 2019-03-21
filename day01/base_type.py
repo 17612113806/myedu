9bf3a27e11a9cd74afe30b1273561704222a2376
unknown <15021290281@163.com>
2019-03-21 15:39:42 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,43 @@
+
+#声明一个int_demo 方法
+def int_demo():
+    #声明aint变量 ，并赋值1
+    aint=1
+    #打印aint的值
+    print(aint)
+    #打印 aint 的类型；type（aint）；获取aint的类型
+    print(type(aint))
+
+    # 声明一个str_demo 方法
+def str_demo():
+        # 声明astr变量 ，并赋值1
+        astr= 1
+        # 打印astr的值
+        print(astr)
+        # 打印 astr 的类型；type（astr）；获取astr的类型
+        print(type(astr))
+
+def float_demo():
+        # 声明afloat变量 ，并赋值1 (小数点)
+        afloat = 1.9
+        # 打印afloat的值
+        print(afloat)
+        # 打印 afloat 的类型；type（afloat）；获取afloat的类型
+        print(type(afloat))
+
+
+
+
+
+if __name__ == '__main__':
+    float_demo()
+    #int_demo()
+    #提取变量
+    #result = add(1,2)
+    #print(result)
+    pass
+
+
+
+
+
